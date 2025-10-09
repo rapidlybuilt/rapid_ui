51b32cff7652de7413ea0bf51be4cac83f555c27
dcunning <31681+dcunning@users.noreply.github.com>
2025-10-09 10:02:27 -0400

    sidenav toggle

--- app/javascript/rapid_ui/controllers/layout_controller.js
+++ app/javascript/rapid_ui/controllers/layout_controller.js
@@ -1,7 +1,45 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["sidebar", "toggleButton"]
+  static classes = [ "sidebar-on", "sidebar-open" ]
+
   connect() {
-    console.log("layout_controller connected");
+    // ensure that the toggle button is in the correct state
+    this.updateSidebarToggleButton();
+  }
+
+  get isSidebarOpen() {
+    return this.sidebarTarget.classList.contains(this.sidebarOpenClassWithDefault);
+  }
+
+  get sidebarOnClassWithDefault() {
+    return this.hasSidebarOnClass ? this.sidebarOnClass : "on";
+  }
+
+  get sidebarOpenClassWithDefault() {
+    return this.hasSidebarOpenClass ? this.sidebarOpenClass : "open";
+  }
+
+  toggleSidebar() {
+    if (this.isSidebarOpen) {
+      this.closeSidebar();
+    } else {
+      this.openSidebar();
+    }
+  }
+
+  openSidebar() {
+    this.sidebarTarget.classList.add(this.sidebarOpenClassWithDefault);
+    this.updateSidebarToggleButton();
+  }
+
+  closeSidebar() {
+    this.sidebarTarget.classList.remove(this.sidebarOpenClassWithDefault);
+    this.updateSidebarToggleButton();
+  }
+
+  updateSidebarToggleButton() {
+    this.toggleButtonTarget.classList.toggle(this.sidebarOnClassWithDefault, this.isSidebarOpen);
   }
 }
