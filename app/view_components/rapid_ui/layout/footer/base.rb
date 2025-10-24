7eda19d4f3b478558bd1d1b3d6a11a1bd4c1d71d
dcunning <31681+dcunning@users.noreply.github.com>
2025-10-24 10:24:16 -0400

    cleaning up and testing footer

--- app/view_components/rapid_ui/layout/footer/base.rb
+++ app/view_components/rapid_ui/layout/footer/base.rb
@@ -2,16 +2,76 @@ module RapidUI
   module Layout
     module Footer
       class Base < ApplicationComponent
-        renders_one :left, Components
-        renders_one :right, Components
+        renders_one :left, ->(**kwargs, &block) do
+          build(Items, tag_name: :div, **kwargs, class: merge_classes("footer-left", kwargs[:class]), &block)
+        end
+
+        renders_one :right, ->(**kwargs, &block) do
+          build(Items, tag_name: :div, **kwargs, class: merge_classes("footer-right", kwargs[:class]), &block)
+        end
 
         def initialize(**kwargs)
           super(tag_name: :footer, **kwargs)
-
-          with_left
-          with_right
-
           yield self if block_given?
+        end
+
+        def call
+          component_tag { safe_join([ left, right ]) }
+        end
+
+        class Items < ApplicationComponent
+          renders_many :items, ->(type, *args, **kwargs, &block) do
+            case type
+            when :copyright
+              build(Copyright, *args, **kwargs, &block)
+            when :text_link
+              text, path = *args
+              build(Button, build(Tag).with_content(text), path:, **kwargs, class:  merge_classes("footer-btn", kwargs[:class]), &block)
+            when :icon_link
+              icon, path = *args
+              build(Button, build(Icon, icon), path:, **kwargs, class:  merge_classes("footer-btn", kwargs[:class]), &block)
+            when :tag
+              build(Tag, *args, **kwargs, &block)
+            else
+              raise ArgumentError, "invalid item type: #{type}"
+            end
+          end
+
+          def with_copyright(*args, **kwargs, &block)
+            with_item(:copyright, *args, **kwargs, &block)
+          end
+
+          def with_text_link(*args, **kwargs, &block)
+            with_item(:text_link, *args, **kwargs, &block)
+          end
+
+          def with_icon_link(*args, **kwargs, &block)
+            with_item(:icon_link, *args, **kwargs, &block)
+          end
+
+          def with_tag(*args, **kwargs, &block)
+            with_item(:tag, *args, **kwargs, &block)
+          end
+
+          def build_copyright(*args, **kwargs, &block)
+            build_item(:copyright, *args, **kwargs, &block)
+          end
+
+          def build_text_link(*args, **kwargs, &block)
+            build_item(:text_link, *args, **kwargs, &block)
+          end
+
+          def build_icon_link(*args, **kwargs, &block)
+            build_item(:icon_link, *args, **kwargs, &block)
+          end
+
+          def build_tag(*args, **kwargs, &block)
+            build_item(:tag, *args, **kwargs, &block)
+          end
+
+          def call
+            component_tag { safe_join(items) }
+          end
         end
       end
     end
---
+++ test/view_components/rapid_ui/layout/footer/copyright_test.rb
@@ -0,0 +1,21 @@
+require_relative "../../view_component_test_case"
+
+module RapidUI
+  module Layout
+    module Footer
+      class CopyrightTest < ViewComponentTestCase
+        described_class Copyright
+
+        test "renders a basic copyright notice" do
+          render_inline(build(start_year: 2020, end_year: 2025, company_name: "Test Company"))
+          assert_equal %(<span>&copy; 2020-2025, Test Company</span>), rendered_content
+        end
+
+        test "escapes everything properly" do
+          render_inline(build(start_year: "20<20", end_year: "20>25", company_name: "<script>alert('test')</script>"))
+          assert_equal %(<span>&copy; 20&lt;20-20&gt;25, &lt;script&gt;alert(&#39;test&#39;)&lt;/script&gt;</span>), rendered_content
+        end
+      end
+    end
+  end
+end
