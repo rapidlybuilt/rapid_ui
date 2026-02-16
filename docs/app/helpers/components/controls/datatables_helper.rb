a395106cb945ed14edc9cec9d11913eae5cae9f4
dcunning <31681+dcunning@users.noreply.github.com>
2026-02-16 09:10:05 -0500

    docs cleanup

--- docs/app/helpers/components/controls/datatables_helper.rb
+++ docs/app/helpers/components/controls/datatables_helper.rb
@@ -5,10 +5,8 @@ module Components::Controls::DatatablesHelper
       html: render(@full_example_table),
       tabs: [
         Demo::Tab.new(id: "table", label: "Table", code: CodeBlock.build_from_constant(CountriesTable, factory: ui.factory), current: true),
-        Demo::Tab.new(id: "model", label: "Model", code: CodeBlock.build_from_constant(Country, factory: ui.factory)),
-        # TODO: extract the source from the controller methods directly
         Demo::Tab.new(id: "controller", label: "Controller", code: CodeBlock.build_from_constant(controller.class, factory: ui.factory)),
-        Demo::Tab.new(id: "view", label: "View", code: CodeBlock.new(%(render @countries_table), language: "ruby", factory: ui.factory)),
+        Demo::Tab.new(id: "model", label: "Model", code: CodeBlock.build_from_constant(Country, factory: ui.factory)),
       ],
     )
   end
